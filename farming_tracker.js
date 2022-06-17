8b575ce304eb21212f53360e2cac69440adf0c38
IceFreez3r <39794212+IceFreez3r@users.noreply.github.com>
2022-06-17 14:34:45 +0200

    Empty boxes for every missing seed

--- farming_tracker.js
+++ farming_tracker.js
@@ -3,35 +3,33 @@ function farmingTracker() {
     if (seedContainer.classList.contains("processed")) {
         return;
     }
+    let existingSeeds = {};
+    for (let i = 0; i < seeds.length; i++) {
+        console.log(seeds[i]);
+        existingSeeds[seeds[i]] = false;
+    }
     let seedList = seedContainer.getElementsByClassName("item");
-    let existingSeeds = [[false, false, false, false], [false, false, false, false], [false, false, false, false], [false, false, false, false]];
-    // Add classes to seeds
     for (let i = 0; i < seedList.length; i++) {
         let seed = seedList[i];
-        const seedName = seed.getAttribute("data-for").replace(/farming\-seeds[0-9]+/, "");
-        // check in which list the seed name is
-        if (seedName === mysteriousSeedName) {
-            const mysteriousSeedSize = seed.getElementsByClassName("item-augment")[0].innerText;
-            let [size_1, size_2] = mysteriousSeedSize.split("x");
-            existingSeeds[size_1 - 1][size_2 - 1] = true;
-            seed.parentNode.style.gridArea = "mysterious-seed-" + mysteriousSeedSize;
-        } else if (singleSlotSeedNames.indexOf(seedName) !== -1) {
-            seed.parentNode.classList.add("single-slot-seed");
-        } else if (multiSlotSeedNames.indexOf(seedName) !== -1) {
-            seed.parentNode.classList.add("multi-slot-seed");
-        } else {
-            console.log("Unknown seed name: " + seedName);
+        let seedName = seed.getAttribute("data-for")
+                           .replace("farming-seeds", "")
+                           .replace(/[0-9]/g, "")
+                           .replaceAll(" ", "-")
+                           .toLowerCase();
+        if (seedName === "mysterious-seed") {
+            // Add mysterious seed size to name
+            seedName += "-" + seed.getElementsByClassName("item-augment")[0].innerText;
         }
+        existingSeeds[seedName] = true;
+        seed.parentNode.style.gridArea = seedName;
     }
-    // Add pseudo mysterious seed divs if the user doesn't have that type
-    for (let i = 0; i < 4; i++) {
-        for (let j = 0; j < 4; j++) {
-            if (!existingSeeds[i][j]) {
-                console.log(i, j)
-                seedContainer.insertAdjacentHTML("beforeend", `<div style="grid-area: mysterious-seed-${i+1}x${j+1};" class="fake-item"></div>`);
-            }
+    // Add empty boxes for missing seeds
+    Object.entries(existingSeeds).forEach(([seedName, exists]) => {
+        console.log(seedName);
+        if (!exists) {
+            seedContainer.insertAdjacentHTML("beforeend", `<div style="grid-area: ${seedName};" class="fake-item"></div>`);
         }
-    }
+    });    
     // Remove old header
     seedContainer.parentNode.getElementsByClassName("farming-seeds-title")[0].remove();
     seedContainer.parentNode.getElementsByClassName("farming-seeds-title-border")[0].remove();
@@ -51,28 +49,39 @@ function farmingTracker() {
     seedContainer.classList.add("processed");
 }
 
-const mysteriousSeedName = "Mysterious Seed";
-const singleSlotSeedNames = [
-    "Carrot Seed",
-    "Potato Seed",
-    "Wheat Seed",
-    "Tomato Seed",
-    "Mushroom Spore",
-    "Chili Pepper Seed",
-    "Sugarcane Seed",
-    "Pumpkin Seed",
-    "Rice Seed",
-    "Peppercorn Seed",
-];
-const multiSlotSeedNames = [
-    "Wildberry Bush Seed",
-    "Tree Seed",
-    "Oak Tree Seed",
-    "Apple Tree Seed",
-    "Banana Tree Seed",
-    "Sageberry Bush Seed",
-    "Maple Tree Seed",
-    "Willow Tree Seed",
-    "Yew Tree Seed",
-    "Elder Tree Seed",
-];
+const seeds = ["mysterious-seed-1x1",
+               "mysterious-seed-1x2",
+               "mysterious-seed-1x3",
+               "mysterious-seed-1x4",
+               "mysterious-seed-2x1",
+               "mysterious-seed-2x2",
+               "mysterious-seed-2x3",
+               "mysterious-seed-2x4",
+               "mysterious-seed-3x1",
+               "mysterious-seed-3x2",
+               "mysterious-seed-3x3",
+               "mysterious-seed-3x4",
+               "mysterious-seed-4x1",
+               "mysterious-seed-4x2",
+               "mysterious-seed-4x3",
+               "mysterious-seed-4x4",
+               "carrot-seed",
+               "potato-seed",
+               "wheat-seed",
+               "tomato-seed",
+               "mushroom-spore",
+               "sugarcane-seed",
+               "chili-pepper-seed",
+               "rice-seed",
+               "pumpkin-seed",
+               "peppercorn-seed",
+               "wildberry-bush-seed",
+               "sageberry-bush-seed",
+               "tree-seed",
+               "oak-tree-seed",
+               "willow-tree-seed",
+               "banana-tree-seed",
+               "apple-tree-seed",
+               "maple-tree-seed",
+               "yew-tree-seed",
+               "elder-tree-seed"]
