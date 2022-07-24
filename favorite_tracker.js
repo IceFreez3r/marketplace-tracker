faa946e420060808800ff3d8a93d508a84111c33
IceFreez3r <39794212+IceFreez3r@users.noreply.github.com>
2022-07-24 16:16:59 +0200

    Filter favorite button

--- favorite_tracker.js
+++ favorite_tracker.js
@@ -1,6 +1,7 @@
 class FavoriteTracker {
     constructor() {
         this.loadData();
+        this.filterActive = false;
 
         this.observer = new MutationObserver(mutations => {
             const selectedSkill = document.getElementsByClassName('nav-tab-left noselect selected-tab')[0];
@@ -8,6 +9,7 @@ class FavoriteTracker {
                 return;
             }
             if (selectedSkill.innerText !== 'Marketplace') {
+                this.filterActive = false;
                 return;
             }
             this.favoriteTracker();
@@ -25,6 +27,8 @@ class FavoriteTracker {
         let items = document.getElementsByClassName('marketplace-sell-items')[0];
         if (items) {
             this.highlightFavorites(items);
+            this.filterFavoritesButton();
+            this.filterFavorites();
             return;
         }
         // Overview Page
@@ -32,6 +36,8 @@ class FavoriteTracker {
         if (items) {
             items = items.firstChild;
             this.highlightFavorites(items);
+            this.filterFavoritesButton();
+            this.filterFavorites();
             return;
         }
         // Buy page
@@ -39,6 +45,39 @@ class FavoriteTracker {
         if (buyHeader) {
             this.toggleFavoriteButton(buyHeader.parentNode);
             return;
+        }
+    }
+
+    filterFavoritesButton() {
+        let filter = document.getElementById('tracker-favorite-filter');
+        if (filter) {
+            const daelsFilter = document.querySelector('.daelis-marketplace-filter:not(.recognized)');
+            if (daelsFilter) {
+                filter.remove();
+                daelsFilter.classList.add('recognized');
+            } else {
+                return;
+            }
+        }
+        const sortingContainer = document.getElementsByClassName('market-sorting-container')[0];
+        saveInsertAdjacentHTML(sortingContainer.firstChild, 'afterend', `
+<div id="tracker-favorite-filter" class="${this.filterActive ? "filter-active" : ""}">
+    ${this.favoriteTemplate("tracker-favorite-filter-svg")}
+</div>
+        `);
+        filter = document.getElementById('tracker-favorite-filter');
+        const that = this;
+        filter.addEventListener('click', function () {
+            filter.classList.toggle('filter-active');
+            that.filterActive = !that.filterActive;
+            that.filterFavorites();
+        });
+    }
+
+    filterFavorites() {
+        let notFavorites = document.querySelectorAll('.marketplace-content .item:not(.favorite-highlight)');
+        for (let i = 0; i < notFavorites.length; i++) {
+            notFavorites[i].parentNode.classList.toggle('hidden', this.filterActive);
         }
     }
 
@@ -78,9 +117,9 @@ class FavoriteTracker {
         });
     }
 
-    favoriteTemplate() {
+    favoriteTemplate(classes = "") {
         return `
-<svg stroke="yellow" stroke-width="30px" x="0px" y="0px" width="24px" heigth="24px" viewBox="-15 -10 366 366">
+<svg class="${classes}" stroke="yellow" stroke-width="30px" x="0px" y="0px" width="24px" heigth="24px" viewBox="-15 -10 366 366">
     <path d="M329.208,126.666c-1.765-5.431-6.459-9.389-12.109-10.209l-95.822-13.922l-42.854-86.837  c-2.527-5.12-7.742-8.362-13.451-8.362c-5.71,0-10.925,3.242-13.451,8.362l-42.851,86.836l-95.825,13.922  c-5.65,0.821-10.345,4.779-12.109,10.209c-1.764,5.431-0.293,11.392,3.796,15.377l69.339,67.582L57.496,305.07  c-0.965,5.628,1.348,11.315,5.967,14.671c2.613,1.899,5.708,2.865,8.818,2.865c2.387,0,4.784-0.569,6.979-1.723l85.711-45.059  l85.71,45.059c2.208,1.161,4.626,1.714,7.021,1.723c8.275-0.012,14.979-6.723,14.979-15c0-1.152-0.13-2.275-0.376-3.352  l-16.233-94.629l69.339-67.583C329.501,138.057,330.972,132.096,329.208,126.666z">
 </svg>
         `;
