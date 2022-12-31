cb49cfe0e4369be3bc6d2ced3fcdac6b5a5596ea
IceFreez3r <39794212+IceFreez3r@users.noreply.github.com>
2022-12-31 23:06:13 +0100

    Versionnumber, README

--- marketplace_tracker.user.js
+++ marketplace_tracker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Idlescape Marketplace Tracker
 // @namespace    https://github.com/IceFreez3r
-// @version      1.0.5
+// @version      1.0.6
 // @description  Automatically tracks prices of items on the Idlescape Marketplace
 // @author       IceFreez3r
 // @match        https://*.idlescape.com/game
